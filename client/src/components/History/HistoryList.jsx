5d9a5f47c0dc0aff35637d4d45c73025a6980dd7
Kin Chan <xkinchanx@gmail.com>
2018-03-09 13:48:30 -0800

    [update]: Display algorithum update to display both wins and losses

--- client/src/components/History/HistoryList.jsx
+++ client/src/components/History/HistoryList.jsx
@@ -8,15 +8,18 @@ export const HistoryList = ({ history, userInfo }) => {
   return (
     <div>
       {history.map( (hist, i) => {
-      outcome = hist.outcome === 0 ? 'Loss' : 'Win';
-      return (
-        <div className="outcomes" key={i}>
-          <div>Outcome: {outcome}</div>
-          <div>Opponent: {hist.receiver.username}</div>
-          <div>Time: {hist.time}</div>
-          <div>Clout Earned: {hist.outcome === 0 ? 0 : hist.clout}</div>
-        </div>
-      )})}
+        if (hist.user_id === Number(localStorage.id)) {
+          outcome = hist.outcome === 0 ? 'Loss' : 'Win';
+          return (
+            <div className="outcomes" key={i}>
+              <div>Outcome: {outcome}</div>
+              <div>Opponent: {hist.receiver.username}</div>
+              <div>Time: {hist.time}</div>
+              <div>Clout Earned: {hist.outcome === 0 ? 0 : hist.clout}</div>
+            </div>
+          )
+        }
+      })}
     </div>
   )
 };
