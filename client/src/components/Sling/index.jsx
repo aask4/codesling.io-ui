813141d89de26cd71b72206faa334fe6cfee675c
Aaron Valdez <valdez2010@gmail.com>
2018-03-08 16:55:06 -0800

    Added challenge title to socket query.

--- client/src/components/Sling/index.jsx
+++ client/src/components/Sling/index.jsx
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
-import io from 'socket.io-client/dist/socket.io.js';
+import React, { Component } from "react";
+import io from "socket.io-client/dist/socket.io.js";
 
-import Sling from './Sling.jsx';
+import Sling from "./Sling.jsx";
 
 class SlingIndex extends Component {
-  state = { 
-    socket: null,
-   }
+  state = {
+    socket: null
+  };
 
   componentWillMount() {
-    this.socket = io('http://localhost:4155', {
+    const challenge =
+      typeof this.props.location.state.challenge === "string"
+        ? JSON.parse(this.props.location.state.challenge)
+        : {title: ''};
+    this.socket = io("http://localhost:4155", {
       query: {
-        roomId: this.props.location.pathname.slice(1)
+        roomId: this.props.location.pathname.slice(1),
+        title: challenge.title
       }
     });
 
@@ -21,12 +26,13 @@ class SlingIndex extends Component {
   render() {
     if (this.props.location.state) {
       return (
-        <Sling socket={this.state.socket} challenge={this.props.location.state.challenge}/>
+        <Sling
+          socket={this.state.socket}
+          challenge={this.props.location.state.challenge}
+        />
       );
     } else {
-      return (
-        <Sling socket={this.state.socket} challenge={{}}/>
-      );
+      return <Sling socket={this.state.socket} challenge={{}} />;
     }
   }
 }
