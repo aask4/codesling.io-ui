b761aa677b7876061e31893aff08de4fe91ad668
qiang song <qiangsong889@yahoo.com>
2018-03-09 10:06:43 -0800

    merge conflicts

--- client/src/components/Challenge/AddChallenge/index.jsx
+++ client/src/components/Challenge/AddChallenge/index.jsx
@@ -128,8 +128,12 @@ if (expected === actual) {
           </div>
         </form>
 <<<<<<< HEAD
+<<<<<<< HEAD
       </div>
     )
+=======
+      </div>)
+>>>>>>> fixed syntax err
 =======
       </div>)
 >>>>>>> fixed syntax err
